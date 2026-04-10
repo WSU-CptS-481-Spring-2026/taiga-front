cf5009f7e2e9fddb53e643bea3257dba92a8354a
Tman laptop <pogrebinsky2010@gmail.com>
2026-04-10 13:16:02 -0700

    Updating karma, package-lock from npm install

--- karma.conf.js
+++ karma.conf.js
@@ -63,7 +63,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', "coverage"],
+    coverageReporter: {
+    type: 'lcov',
+    dir: 'coverage/',
+    subdir: '.'
+   },
 
 
     // web server port
